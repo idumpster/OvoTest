49c6ce9353f97a8d7fafb337e2d2f872eece3e13
Arie <nttispay@gmail.com>
2023-10-04 02:54:30 +0700

    good night, help :c

--- src/arie/test/Main.php
+++ src/arie/test/Main.php
@@ -3,17 +3,20 @@ declare(strict_types=1);
 
 namespace arie\test;
 
+use galaxygames\ovommand\enum\EnumManager;
+use galaxygames\ovommand\enum\HardEnum;
 use galaxygames\ovommand\OvommandHook;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
-use shared\galaxygames\ovommand\GlobalHookPool;
 
 class Main extends PluginBase implements Listener{
 
 	public function onEnable() : void{
 		OvommandHook::register($this);
 		Server::getInstance()->getPluginManager()->registerEvents($this, $this);
+
+		EnumManager::getInstance()->register(new HardEnum("sound", ["meow" => "cat", "goof" => "dog"]));
 		
 //		var_dump(GlobalHookPool::getHooks());
 //		var_dump($hook = GlobalHookPool::getHook($this)::getOwnedPlugin());
