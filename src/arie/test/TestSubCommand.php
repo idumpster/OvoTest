3d8be1c404121981c4dd6345d82a60d7026270d7
Arie <nttispay@gmail.com>
2023-10-22 22:58:33 +0700

    oof

--- src/arie/test/TestSubCommand.php
+++ src/arie/test/TestSubCommand.php
@@ -10,6 +10,7 @@ use galaxygames\ovommand\parameter\EnumParameter;
 use galaxygames\ovommand\parameter\PositionParameter;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
+use pocketmine\Server;
 
 class TestSubCommand extends BaseSubCommand{
 	public function onRun(CommandSender $sender, string $label, array $args, string $preLabel = "") : void{
@@ -18,6 +19,12 @@ class TestSubCommand extends BaseSubCommand{
 		}
 		if ($sender instanceof Player) {
 			$sender->sendMessage(print_r($args, true));
+		}
+		if (isset($args["b"], $args["online"], $args["pmGamemode"])) {
+			$player = Server::getInstance()->getPlayerExact($args['online']);
+			if ($player !== null) {
+				$player->setGamemode($args["pmGamemode"]);
+			}
 		}
 		var_dump($args);
 	}
@@ -31,6 +38,10 @@ class TestSubCommand extends BaseSubCommand{
 			new PositionParameter("a"),
 			new EnumParameter("aa", DefaultEnums::PM_GAMEMODE),
 		);
-		$this->registerParameters(new BooleanParameter("yes madam"));
+		$this->registerParameters(
+			new PositionParameter("b"),
+			new EnumParameter("online", DefaultEnums::ONLINE_PLAYERS, true),
+			new EnumParameter("pmGamemode", DefaultEnums::PM_GAMEMODE, false)
+		);
 	}
 }
